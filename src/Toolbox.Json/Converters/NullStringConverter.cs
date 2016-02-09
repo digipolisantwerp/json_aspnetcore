d1b4b85314536eb5ed05b61a103a76ed72d7e53e
kstr <info@elkit.be>
2016-02-09 12:41:27 +0100

    2 Converters added.

---
+++ src/Toolbox.Json/Converters/NullStringConverter.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Salga.ServiceAgents.Converters
+{
+    public class NullStringConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            object retVal = new Object();
+            if (reader.Value == null) return null;
+            if (reader.TokenType == JsonToken.StartObject || reader.TokenType == JsonToken.String)
+            {
+                if (reader.Value.ToString() == "null")
+                {
+                    retVal = null;
+                }
+                else
+                {
+                    retVal = reader.Value;
+                }
+            }
+            else if (reader.TokenType == JsonToken.StartArray)
+            {
+                retVal = serializer.Deserialize(reader, objectType);
+            }
+            return retVal;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            if (typeof(String) == objectType) return true;
+            return false;
+        }
+    }
+}
---
+++ src/Toolbox.Json/Converters/SingleValueArrayConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Salga.ServiceAgents.Converters
+{
+    public class SingleValueArrayConverter<T> : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            object retVal = new Object();
+            if (reader.TokenType == JsonToken.StartObject || reader.TokenType == JsonToken.String)
+            {
+                T instance = (T)serializer.Deserialize(reader, typeof(T));
+                retVal = new List<T>() { instance };
+            }
+            else if (reader.TokenType == JsonToken.StartArray)
+            {
+                retVal = serializer.Deserialize(reader, objectType);
+            }
+            return retVal;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return false;
+        }
+    }
+}
